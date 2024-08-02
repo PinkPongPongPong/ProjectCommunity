35191ff6a7bea94c8172b57d10232b9ab531fa17
kim <modan8199@gmail.com>
2024-08-02 17:34:57 +0900

    first commit

---
+++ src/test/java/com/ohgiraffers/projectgin/ProjectginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.projectgin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
