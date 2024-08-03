7145d10b30d988ed084c122a700a41ff17a9d1af
weed97 <weed9935@gmal.com>
2024-08-03 13:34:14 +0900

    BoardDTO와 Board 엔티티 관계 및 테이블 설정 DTO 생성

--- src/main/java/com/ohgiraffers/projectgin/model/dto/BoardDTO.java
+++ src/main/java/com/ohgiraffers/projectgin/model/dto/BoardDTO.java
@@ -1,4 +1,24 @@
 package com.ohgiraffers.projectgin.model.dto;
 
+import lombok.*;
+
+import java.sql.Date;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
 public class BoardDTO {
+
+    private String post_Id;
+    private String user_NickName;
+    private String post_Title;
+    private Date category_Date;
+    private int views;
+    private int like;
+    private String division;
+    private int hate;
+
+
 }
